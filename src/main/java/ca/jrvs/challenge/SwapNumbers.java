6c3a36696c9aeccfc348ac48a7056b5a6f8ae81e
Zohra_Hlimi <zohra.hlimi.jrvs.ca@gmail.com>
2019-07-28 01:58:11 +0000

    new updates

--- src/main/java/ca/jrvs/challenge/SwapNumbers.java
+++ src/main/java/ca/jrvs/challenge/SwapNumbers.java
@@ -1,4 +1,14 @@
 package ca.jrvs.challenge;
 
 public class SwapNumbers {
+    public static void function (int a , int b){
+        a = a+b;
+        b = a-b;
+        a = a-b;
+        System.out.println("a: "+a  +" b: "+ b);
+    }
+
+    public static void main(String[] args) {
+        function(1,2);
+    }
 }
