dc49d921679d55ef70ff94a50c22eecfc29e6611
Zohra_Hlimi <zohra.hlimi.jrvs.ca@gmail.com>
2019-07-29 20:28:02 +0000

    last updates

--- src/main/java/ca/jrvs/challenge/CountPrimes.java
+++ src/main/java/ca/jrvs/challenge/CountPrimes.java
@@ -2,7 +2,15 @@ package ca.jrvs.challenge;
 
 import java.util.ArrayList;
 
+
 public class CountPrimes {
+    /**
+     * Checks if number is prime
+     * update count if it is prime
+     * add number to the list if prime
+     * @param n
+     * @return total number of prime numbers smaller than n.
+     */
     public static int countPrimes(int n){
         ArrayList<Integer> primes = new ArrayList<>();
         int count = 0;
@@ -12,11 +20,16 @@ public class CountPrimes {
                 primes.add(i);
 
             }
-
         }
         System.out.println(primes);
         return count ;
     }
+
+    /**
+     * checks if an int number is prime
+     * @param num
+     * @return True if number is prime
+     */
     protected static boolean IsPrime(int num){
         int count = 0;
         for (int i =1 ;i<=num ; i++){
